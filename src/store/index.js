879b06d446eb8e07903af0213f7b8c82cbaf491e
AleksandrT <drink0.5l@gmail.com>
2021-04-08 08:32:15 +0300

    fixed create store
    fix

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { combineReducers, createStore } from "redux";
 import { chatReducer } from "./chats/reducer.js";
 
 export const store = createStore(
-    combineReducers(
-        chatReducer,
-    )
+    combineReducers({
+        chat: chatReducer,
+    })
 );
