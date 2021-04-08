898bd8e64e299f9b9438b39a234e2994bbe7772a
AleksandrT <drink0.5l@gmail.com>
2021-04-08 12:19:46 +0300

    fix redux devtools load

--- src/store/index.js
+++ src/store/index.js
@@ -9,5 +9,5 @@ export const store = createStore(
         chat: chatReducer,
         message: messageReducer,
         profile: profileReducer,
-    }), window.__REDUX_DEVTOOLS_EXTENSION__()
+    }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
