9f341f199f5cad5cd90e7a56c148a114818ecf2f
AleksandrT <drink0.5l@gmail.com>
2021-04-08 05:56:35 +0300

    remove excess import

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Messenger from "./components/Messenger.jsx";
 import MainRouter from "./components/MainRouter.jsx";
 
 import "./css/normalize.css";
