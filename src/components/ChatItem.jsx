4300142dca2dabcc4feffb3118f58831fbe13e70
AleksandrT <drink0.5l@gmail.com>
2021-04-12 12:40:56 +0300

    add new message notification

--- src/components/ChatItem.jsx
+++ src/components/ChatItem.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { ListItem, ListItemText } from "@material-ui/core";
 import { useHistory, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-//import "../css/chat_list-item.scss";
+import "../css/chat_list-item.scss";
 
 const ChatItem = (props) => {
     const { title, id } = props.entity;
     const { chatId } = useParams();
     const history = useHistory();
+    const notificationList = useSelector(store => store.chat.chatNotificationList);
 
     const onClickHandler = () => {
         history.push(`/chat/${id}`);
     }
 
     return (
-        <ListItem button onClick={onClickHandler} selected={chatId === id}>
+        <ListItem button className={notificationList.includes(id) ? "notification" : ""} onClick={onClickHandler} selected={chatId === id}>
             <ListItemText primary={title} />
         </ListItem>
     )
--- src/store/message/actions.js
+++ src/store/message/actions.js
@@ -1,11 +1,21 @@
 import { ADD_MESSAGE } from "./types.js";
+import { addNotificationAction, removeNotificationAction } from "../chat/actions.js";
 import Bot from "../../Bot.js";
 
 const addMessage = (chatId, message) => ({ type: ADD_MESSAGE, payload: { chatId: chatId, message: message } });
 
 export const addMessageAction = (chatId, message) => (dispatch) => {
-    dispatch(addMessage(chatId, message));
+    if (message.isUser) {
+        setTimeout(() => {
+            dispatch(addMessageAction(chatId, Bot.getOpinion(message.text)));
+        }, 2000);
+    }
+    else {
+        dispatch(addNotificationAction(chatId));
+        setTimeout(() => {
+            dispatch(removeNotificationAction(chatId))
+        }, 3000);
+    }
 
-    if (message.isUser)
-        dispatch(addMessage(chatId, Bot.getOpinion(message.text)));
+    dispatch(addMessage(chatId, message));
 }
