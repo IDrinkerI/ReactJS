3e91e27680e11ff3d58b4a5a38ed376741581ece
AleksandrT <drink0.5l@gmail.com>
2021-04-12 10:29:45 +0300

    the bot answers now in the middleware

--- src/store/message/actions.js
+++ src/store/message/actions.js
@@ -1,3 +1,11 @@
 import { ADD_MESSAGE } from "./types.js";
+import Bot from "../../Bot.js";
 
-export const addMessageAction = (chatId, message) => ({ type: ADD_MESSAGE, payload: { chatId: chatId, message: message } });
+const addMessage = (chatId, message) => ({ type: ADD_MESSAGE, payload: { chatId: chatId, message: message } });
+
+export const addMessageAction = (chatId, message) => (dispatch) => {
+    dispatch(addMessage(chatId, message));
+
+    if (message.isUser)
+        dispatch(addMessage(chatId, Bot.getOpinion(message.text)));
+}
